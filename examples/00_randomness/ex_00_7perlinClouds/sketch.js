4a00686dc184ecb1930198aa79c98e0314018dfb
Danny Platt <daniel.platt.dp@gmail.com>
2025-01-05 11:35:26 -0700

    update website

--- examples/00_randomness/ex_00_7perlinClouds/sketch.js
+++ examples/00_randomness/ex_00_7perlinClouds/sketch.js
@@ -4,7 +4,6 @@
 
 let xoff = 0;
 let yoff = 0;
-let toff = 0;
 
 function setup() {
   createCanvas(600, 400);
@@ -15,12 +14,12 @@ function draw() {
   loadPixels();
   yoff = 0;
   for (var y = 0; y < width; y++){
-    xoff = toff;
+    xoff = 0;
     for (var x = 0; x < width; x++){
       noiseDetail(2);
-      let brightness = map(noise(xoff, yoff, toff), 0, 1, 0, 255);
+      let brightness = map(noise(xoff, yoff), 0, 1, 0, 255);
       let index = (x + y * width) * 4;
-      pixels[index + 0] = map(noise(yoff, yoff, toff), 0, 1, 0, 255);
+      pixels[index + 0] = map(noise(yoff, yoff), 0, 1, 0, 255);
       pixels[index + 1] = brightness;
       pixels[index + 2] = brightness;
       pixels[index + 3] = 255;
@@ -29,6 +28,6 @@ function draw() {
     yoff += 0.005;
   }
   updatePixels();
-  toff += 0.01;
+  noLoop();
 }
 
